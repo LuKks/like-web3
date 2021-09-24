adf82a4858db069fc919869486116fcb74b21b3a
Lucas Barrena <lucasbarrena98@gmail.com>
2021-09-24 17:50:05 -0300

    base

---
+++ README.md
@@ -0,0 +1,145 @@
+# like-web3
+
+Common Web3/BSC for Node.js
+
+![](https://img.shields.io/npm/v/like-web3.svg) ![](https://img.shields.io/npm/dt/like-web3.svg) ![](https://img.shields.io/badge/tested_with-tap-e683ff.svg) ![](https://img.shields.io/github/license/LuKks/like-mysql.svg)
+
+```javascript
+const Web3 = require('like-web3');
+
+const web3 = new Web3({
+  providers: ['wss://bsc-ws-node.nariox.org:443'],
+  privateKey: '0x...'
+});
+
+console.log(await web3.contract('WBNB').totalSupply());
+```
+
+## Install
+```
+npm i like-web3
+```
+
+## Providers
+#### Mainnet
+```
+https://bsc-dataseed.binance.org
+wss://bsc-ws-node.nariox.org:443
+```
+
+Looking for a stable WebSocket provider? I recommend to create an account here:\
+https://admin.moralis.io/register
+Then you create an endpoint like this one:\
+`wss://speedy-nodes-nyc.moralis.io/d4684f47341b442165fc8c9e/bsc/mainnet/ws`
+
+#### Testnet
+```
+https://data-seed-prebsc-1-s1.binance.org:8545
+```
+Moralis.io also gives you a testnet WebSocket provider.
+
+#### Notes
+It only supports HTTP and WebSocket providers.
+The default provider is `https://bsc-dataseed.binance.org`.
+
+## Examples
+#### Setup testnet
+```javascript
+const web = new Web3({
+  providers: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+  testnet: true, // this will use a different network/chain id
+  privateKey: '0x...'
+});
+```
+
+#### Swap tokens in PancakeSwap
+```javascript
+let tx = await web3.transaction('PANCAKESWAP_ROUTER', {
+  method: 'swapExactTokensForTokens',
+  args: [
+    web3.toWei(0.1, 18), // amountIn (WBNB, 18 decimals)
+    web3.toWei(1.89, 18), // amountOutMin (CAKE, 18 decimals)
+    [web3.CONTRACTS.WBNB.address, web3.CONTRACTS.CAKE.address], // path (trade route)
+    web.wallet.address, // to
+    Math.round(Date.now() / 1000) + 120, // deadline
+  ],
+  to: 'PANCAKESWAP_ROUTER',
+  nonce: 1
+});
+console.log('here tx is valid and it would succeed (estimate gas limit not failed)');
+
+tx = await tx.send();
+console.log('swap hash', tx.transactionHash);
+
+let receipt = await tx.wait(); // wait for 1 confirmation and return tx details
+console.log('receipt', receipt.status);
+```
+
+#### One line method call
+```javascript
+await web3.contract('0xa1b2c3').name();
+await web3.contract('0xa1b2c3').decimals();
+await web3.contract('0xa1b2c3').totalSupply();
+await web3.contract('PANCAKESWAP_FACTORY').getPair(['0x123', '0x456']);
+await web3.contract('PANCAKESWAP_ROUTER').getAmountsOut(amountIn, [from, to]);
+```
+
+#### Re use same contract
+```javascript
+let cakeContract = web3.contract('0xa1b2c3');
+await cakeContract.name();
+await cakeContract.decimals();
+await cakeContract.totalSupply();
+```
+
+#### Watch pending transactions
+```javascript
+// requires a websocket provider
+let subscription = web3.subscribePendingTransactions({ intervalMs: 50 });
+
+web3.on('pendingTransactions', (err, transactionHash, tx) => {
+  if (err) throw err;
+  console.log(transactionHash, 'tx', tx);
+});
+
+// subscription.unsubscribe();
+```
+
+#### Util
+```javascript
+// wei
+web3.toWei('0.01', 18); // ie. WBNB (18 decimals) => 10000000000000000
+web3.fromWei('10000000000000000', 18); // ie. WBNB (18 decimals) => 0.01
+
+//
+let method = web3.decodeMethod(tx.input); // => { name: 'swapExactETHForTokens', ... }
+let swap = await web3.decodeSwap({ method, txValue: tx.value }); // => { amountIn: '0.01', ... }
+
+// cached
+await web3.getTokenDecimals('0xa1b2c3'); // => '18'
+await web3.getTokenName('0xa1b2c3'); // => 'Wrapped BNB'
+await web3.getPair('PANCAKESWAP_FACTORY', ['0x123', '0x456']); // => '0x42f6f...'
+```
+
+#### Methods from original web3
+```javascript
+let nonce = await web3.eth.getTransactionCount(web3.wallet.address);
+let receipt = await web3.eth.getTransaction(transactionHash);
+// etc
+```
+
+#### Add more named contracts
+```javascript
+Web3.addContract('PANCAKESWAP_FACTORY', {
+  address: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+  abi: [...]
+});
+```
+
+## Tests
+```
+There are no tests yet.
+```
+
+## License
+Code released under the [MIT License](https://github.com/LuKks/like-web3/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,442 @@
+/*
+ like-web3 (https://npmjs.com/package/like-web3)
+ Copyright 2021 Lucas Barrena
+ Licensed under MIT (https://github.com/LuKks/like-web3)
+*/
+
+'use strict';
+
+// fixbug: Error: Number can only safely store up to 53 bits
+// https://github.com/ChainSafe/web3.js/pull/3948#issuecomment-821779691
+(function () {
+  const fs = require('fs');
+  let path = './node_modules/number-to-bn/node_modules/bn.js/lib/bn.js';
+  let content = fs.readFileSync(path, 'utf8');
+  content = content.replace('assert(false, \'Number can only safely store up to 53 bits\');', 'ret = Number.MAX_SAFE_INTEGER;');
+  fs.writeFileSync(path, content, 'utf8');
+})();
+
+const Web3 = require('web3');
+const Tx = require('ethereumjs-tx').Transaction;
+const EthCommon = require('ethereumjs-common').default;
+const abiDecoder = require('abi-decoder');
+const LikeEmitter = require('like-events');
+const events = require('events');
+const Decimal = require('decimal.js');
+const CONTRACTS = require('./contracts-list.js');
+
+Decimal.set({ precision: 30 });
+Decimal.set({ rounding: Decimal.ROUND_HALF_FLOOR });
+
+abiDecoder.addABI(CONTRACTS.PANCAKESWAP_ROUTER.abi);
+
+module.exports = LikeWeb3;
+
+/*
+const web3 = new Web3({
+  providers: ['wss://bsc-ws-node.nariox.org:443'],
+  // testnet: true, // this will use a different network/chain id
+  privateKey: '0x...'
+});
+*/
+function LikeWeb3 ({ providers, testnet, privateKey }) {
+  if (!(this instanceof LikeWeb3)) {
+    return new LikeWeb3(opts);
+  }
+
+  events.LikeEmitter.call(this);
+
+  // pool of nodes
+  let provider = providers[0];
+
+  // detect provider protocol
+  if (provider.startsWith('http://') || provider.startsWith('https://')) {
+    this.web3 = new Web3(provider);
+  } else if (provider.startsWith('ws://') || provider.startsWith('wss://')) {
+    this.web3 = new Web3(new Web3.providers.WebsocketProvider(provider));
+  } else {
+    throw 'only supports http/s, ws/s';
+  }
+
+  // share the same eth and utils
+  this.eth = this.web3.eth;
+  this.utils = this.web3.utils;
+
+  // network and chain id
+  let blockchainId = testnet ? 97/*testnet*/ : 56/*mainnet*/;
+  this.customChain = EthCommon.forCustomChain('mainnet', {
+    name: 'BSC ' + testnet ? 'Testnet' : 'Mainnet',
+    networkId: blockchainId,
+    chainId: blockchainId
+  }, 'petersburg');
+
+  // support for automatic "from" and sign
+  this.wallet = {
+    address: '',
+    privateKey: ''
+  };
+  if (privateKey) {
+    this.wallet = this.web3.eth.accounts.privateKeyToAccount(privateKey);
+  }
+}
+
+util.inherits(LikeWeb3, events.LikeEmitter);
+
+LikeWeb3.CONTRACTS = CONTRACTS;
+
+LikeWeb3.addContract = function (key, { address, abi, fee }) {
+  CONTRACTS[key] = {
+    address,
+    abi
+  };
+  if (fee) {
+    CONTRACTS[key].fee = fee;
+  }
+}
+
+/*
+let tx = await web3.transaction('PANCAKESWAP_ROUTER', {
+  // abi: [{...}],
+  method: 'swap',
+  args: [],
+  from: '0x123',
+  to: '0x123',
+  // value: 0,
+  nonce: 1,
+  // gasPrice: 5,
+  // gasLimit: 250000,
+  // privateKey: 'hwwwy123'
+});
+tx = await tx.send(); // quickly returns { transactionHash, wait() }
+console.log(await tx.wait()); // wait for 1 confirmation and return transaction details
+*/
+LikeWeb3.prototype.transaction = async function (contract, { abi, method, args, from, to, value, nonce, gasPrice, gasLimit, privateKey }) {
+  let data;
+  if (method) {
+    let CONTRACT = CONTRACTS[contract];
+    if (!CONTRACT) {
+      abi = abi || CONTRACTS.GENERIC_TOKEN.abi;
+      CONTRACT = { abi, address: contract };
+    }
+
+    args = this.argsToHex(args);
+
+    let contractAny = new this.web3.eth.Contract(CONTRACT.abi, CONTRACT.address);
+    data = contractAny.methods[method](...args).encodeABI();
+  }
+
+  if (CONTRACTS[to]) {
+    to = CONTRACTS[to].address;
+  }
+
+  if (!privateKey && this.wallet.privateKey) {
+    if (from !== this.wallet.address) {
+      throw new Error('you need to set "privateKey" of the "from" arg or remove the "from" arg');
+    }
+    privateKey = this.wallet.privateKey;
+    from = this.wallet.address;
+  }
+
+  let transaction = await NewTx({
+    from,
+    to,
+    value: this.web3.utils.toHex(value || 0),
+    data,
+    nonce: this.web3.utils.toHex(nonce),
+    gasPrice,
+    gasLimit,
+  });
+
+  return {
+    send: () => {
+      return this.sendTransaction(transaction, { privateKey });
+    }
+  };
+}
+
+// web3.sendTransaction({ .. }, { privateKey, serialize });
+LikeWeb3.prototype.sendTransaction = async function (transaction, { privateKey, serialize }) {
+  if (!privateKey && this.wallet.privateKey) {
+    privateKey = this.wallet.privateKey;
+  }
+
+  if (privateKey && typeof transaction === 'object') {
+    transaction.sign(Buffer.from(privateKey.slice(2), 'hex'));
+  }
+
+  if (serialize) {
+    if (typeof transaction === 'object') {
+      return '0x' + transaction.serialize().toString('hex');
+    }
+    throw 'transaction is a string, should be an object';
+  }
+
+  let txData = (typeof transaction === 'object') ? ('0x' + transaction.serialize().toString('hex')) : transaction;
+  return await this.broadcast(txData);
+}
+
+// web3.broadcast('0xDaTa..');
+LikeWeb3.prototype.broadcast = async function (txData) {
+  let promise = this.web3.eth.sendSignedTransaction(txData);
+  let transactionHash; // + getting hash should be sync
+
+  // wait and get tx hash
+  promise.once('transactionHash', hash => transactionHash = hash);
+  let started = Date.now();
+  while (Date.now() - started < 15000) {
+    if (transactionHash) {
+      break;
+    }
+    await sleep(50);
+  }
+
+  // + the promise doesn't solve until it has 1 confirmation or it fails (gets rejected)
+
+  return {
+    transactionHash,
+    wait: async () => {
+      try {
+        return await promise;
+      } catch (err) {
+        // console.log('catch send transaction', err);
+        if (!err.receipt) {
+          throw err;
+        }
+        return err.receipt;
+      }
+    }
+  };
+}
+
+// web3.contract('0xa1b2c3').name();
+// web3.contract('0xa1b2c3').decimals();
+// web3.contract('0xa1b2c3').totalSupply();
+// web3.contract('PANCAKESWAP_FACTORY').getPair(['0x123', '0x456']);
+// web3.contract('PANCAKESWAP_ROUTER').getAmountsOut(amountIn, [from, to]);
+LikeWeb3.prototype.contract = function (contractAddress, abi) {
+  let CONTRACT = CONTRACTS[contractAddress];
+  if (!CONTRACT) {
+    abi = abi || CONTRACTS.GENERIC_TOKEN.abi;
+    CONTRACT = { abi, address: contractAddress };
+  }
+
+  let contractAny = new this.web3.eth.Contract(CONTRACT.abi, CONTRACT.address);
+
+  let funcs = {};
+  for (let key in contractAny.methods) {
+    funcs[key] = function () {
+      let args = [...arguments];
+      args = this.argsToHex(args);
+
+      return (contractAny.methods[key].apply(this, args)).call();
+    }
+    contractAny.methods[key] = funcs[key];
+  }
+
+  return funcs;
+}
+
+/*
+let subscription = web3.subscribePendingTransactions({ intervalMs: 50 });
+
+web3.on('pendingTransactions', (err, transactionHash, tx) => {
+  if (err) throw err;
+  console.log(transactionHash, 'tx', tx);
+});
+
+// subscription.unsubscribe();
+*/
+LikeWeb3.prototype.subscribePendingTransactions = function ({ intervalMs }) {
+  intervalMs = intervalMs === undefined ? 50 : intervalMs;
+
+  let batch = null;
+  let batchStarted = 0;
+
+  // subscribe to pending txs
+  let subPendingTxs = this.web3.eth.subscribe('pendingTransactions', async function (err, transactionHash) {
+    if (err) {
+      // console.log('err subscribe', transactionHash, err);
+      throw err;
+    }
+
+    if (!transactionHash) {
+      return;
+    }
+
+    if (!batch) {
+      batch = new this.web3.eth.BatchRequest();
+      batchStarted = Date.now();
+    }
+
+    // get tx details in batches to avoid too many requests
+    batch.add(this.web3.eth.getTransaction.request(transactionHash, function (err, tx) {
+      if (err) {
+        // console.log('err batch add cb', transactionHash, err);
+        // + here should reconnect or something
+        this.emit('pendingTransactions', err, null, null);
+        return;
+      }
+
+      // ignore invalid txs?
+      if (!tx) {
+        return;
+      }
+
+      // emit new pending tx
+      this.emit('pendingTransactions', null, transactionHash, tx);
+    }));
+
+    if (batch.requests.length >= 50 || Date.now() - batchStarted >= intervalMs/*millis*/) {
+      // console.log('total batch requests', batch.requests.length);
+      batch.execute();
+      batch = null;
+    }
+  });
+
+  function unsubscribe () {
+    subPendingTxs.unsubscribe((err, success) => {});
+    /*
+    return new Promise(resolve => {
+      subPendingTxs.unsubscribe((err, success) => {
+        resolve();
+      });
+    });
+    */
+  }
+
+  return { unsubscribe };
+}
+
+LikeWeb3.prototype.NewTx = async function ({ from, to, value, data, nonce, gasPrice, gasLimit }) {
+  // console.log('NewTx', { from, to, value, nonce });
+  let estimatedGasPrice = await this.estimateGasPrice({ from, to, value, data, gasPrice, gasLimit });
+  gasPrice = estimatedGasPrice.gasPrice;
+  gasLimit = estimatedGasPrice.gasLimit;
+  // console.log('before new tx', { gasPrice, gasLimit }); // { gasPrice: '0x12a05f200', gasLimit: '0x1c6e9' }
+  return new Tx({ from, to, value, data, nonce, gasPrice, gasLimit }, { common: BSC_FORK });
+}
+
+LikeWeb3.prototype.estimateGasPrice = async function ({ from, to, value, data, gasPrice, gasLimit }) {
+  // console.log('estimateGasPrice', { from, to, value, data, gasPrice, gasLimit });
+
+  // let realGasPrice = this.web3.utils.fromWei(await this.web3.eth.getGasPrice(), 'gwei'); // '10000000000' -> '10'
+  // console.log('realGasPrice', realGasPrice);
+  if (!gasPrice) {
+    // gasPrice = realGasPrice;
+    gasPrice = this.web3.utils.fromWei(await this.web3.eth.getGasPrice(), 'gwei');
+    // console.log('gasPrice changed', gasPrice);
+  }
+
+  // let realGasLimit = await this.web3.eth.estimateGas({ from, to, value, data }); // 55351
+  // console.log('realGasLimit', realGasLimit);
+  if (!gasLimit) {
+    // gasLimit = realGasLimit;
+    gasLimit = await this.web3.eth.estimateGas({ from, to, value, data });
+    // console.log('gasLimit changed', gasLimit);
+  }
+
+  // console.log('bef gas', { gasPrice, gasLimit });
+  gasPrice = this.web3.utils.toHex(this.web3.utils.toWei(gasPrice, 'gwei'));
+  gasLimit = this.web3.utils.toHex(gasLimit);
+  // console.log('aft gas', { gasPrice, gasLimit });
+  return { gasPrice, gasLimit };
+}
+
+// web3.argsToHex(['1', ['2', '3'], '4']); // support for two depth
+LikeWeb3.prototype.argsToHex = function (args) {
+  return args.map(arg => {
+    if (Array.isArray(arg)) {
+      return arg.map(this.argsToHex);
+    }
+    return this.web3.utils.toHex(arg);
+  });
+}
+
+// web3.toWei('0.01', 18); // ie. WBNB (18 decimals) => 10000000000000000
+LikeWeb3.prototype.toWei = function (amount, decimals) {
+  return new Decimal(amount).mul(10 ** decimals).toFixed(0);
+}
+
+// web3.fromWei('10000000000000000', 18); // ie. WBNB (18 decimals) => 0.01
+LikeWeb3.prototype.fromWei = function (amount, decimals) {
+  return new Decimal(amount).div(10 ** decimals).toFixed();
+}
+
+// let method = web3.decodeMethod(tx.input); // => { name: 'swapExactETHForTokens', ... }
+LikeWeb3.prototype.decodeMethod = function (txInput) {
+  return abiDecoder.decodeMethod(txInput);
+}
+
+// let swap = web3.decodeSwap({ method, txValue: tx.value }); // => { amountIn: '0.01', ... }
+LikeWeb3.prototype.decodeSwap = async function ({ method, txValue }) {
+  // ignore all other methods
+  if (method.name !== 'swapExactETHForTokens' && method.name !== 'SwapExactTokensForTokens') {
+    return;
+  }
+
+  let params = {};
+  method.params.map(param => params[param.name] = param.value);
+  // console.log('decodeSwap params', params);
+
+  let [amountIn, amountOutMin, path, to, deadline] = [0.0, 0.0, [], '', 0];
+
+  let [decimalsIn, decimalsOut] = await Promise.all([
+    this.getTokenDecimals(params.path[0]),
+    this.getTokenDecimals(params.path[1]),
+  ]);
+
+  if (method.name === 'swapExactETHForTokens') {
+    amountIn = this.fromWei(txValue, decimalsIn);
+  } else if (method.name === 'SwapExactTokensForTokens') {
+    amountIn = this.fromWei(params.amountIn, decimalsIn);
+  }
+  amountOutMin = this.fromWei(params.amountOutMin, decimalsOut);
+  path = params.path;
+  to = params.to;
+  deadline = params.deadline;
+
+  // extras
+  let priceMax = new Decimal(amountIn).div(amountOutMin).toFixed();
+
+  return { amountIn, amountOutMin, priceMax, realAmountOut: '0', path, to, deadline };
+}
+
+// web3.getTokenDecimals('0xa1b2c3'); // => '18'
+const cacheDecimals = {};
+LikeWeb3.prototype.getTokenDecimals = async function (address) {
+  if (cacheDecimals[address]) {
+    return cacheDecimals[address];
+  }
+
+  let token = new this.web3.eth.Contract(CONTRACTS.GENERIC_TOKEN.abi, address);
+  let decimals = await token.methods.decimals().call();
+  cacheDecimals[address] = decimals;
+  return decimals;
+}
+
+// web3.getTokenName('0xa1b2c3'); // => 'Wrapped BNB'
+const cacheNames = {};
+LikeWeb3.prototype.getTokenName = async function (address) {
+  if (cacheNames[address]) {
+    return cacheNames[address];
+  }
+
+  let token = new this.web3.eth.Contract(CONTRACTS.GENERIC_TOKEN.abi, address);
+  let name = await token.methods.name().call();
+  cacheNames[address] = name;
+  return name;
+}
+
+// web3.getPair('PANCAKESWAP_FACTORY', ['0x123', '0x456']); // => '0x42f6f...'
+const cachePairs = {};
+LikeWeb3.prototype.getPair = async function (factory, pair) {
+  let key = factory + '-' + pair[0] + '-' + pair[1];
+  if (cachePairs[key]) {
+    return cachePairs[key];
+  }
+
+  let factoryContract = new this.web3.eth.Contract(CONTRACTS[factory].abi, CONTRACTS[factory].address);
+  let pairAddress = await factoryContract.methods.getPair(pair[0], pair[1]).call();
+  cachePairs[key] = pairAddress;
+  return pairAddress;
+}
